aa619c742ad2e883c5bf387323c29cb4b41046f9
Anderson Ramos <andsu.marks.r@gmail.com>
2024-10-27 20:46:32 -0300

    add users service and siginin and siginup dots

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './models/users.model';
+import { AuthService } from 'src/auth/auth.service';
+import { SignupDTO } from './dto/sifnup.dto';
+import { SigninDTO } from './dto/siginin.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel('USer')
+        private readonly usersModel: Model<User>,
+        private readonly authService: AuthService
+    ) {}
+
+    public async signup(signupDTO: SignupDTO): Promise<User> {
+        const user = new this.usersModel(signupDTO);
+        return user.save();
+    };
+
+    public async signin(signinDTO: SigninDTO): Promise<{ name: string; jwtToken: string; email:string }> {
+        
+    }
+}
